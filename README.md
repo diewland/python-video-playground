008f3ee1837077a615542c82041f1c9a5e955319
diewland <diewland@gmail.com>
2019-08-15 17:43:21 +0700

    add ocr from webcam demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Python Video Playground
 
+### Tesseract Installation
+* https://github.com/tesseract-ocr/tesseract/wiki#installation
+
+### References 
 * https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
 * https://github.com/benhowell/examples/blob/master/WebcamStreamingOpenCV/webcam-opencv-example.py
 * https://benhowell.github.io/guide/2015/03/09/opencv-and-web-cam-streaming
---
+++ webcam_and_ocr.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+import pytesseract
+from time import sleep
+
+print('')
+print('Press "q" to stop video')
+print('')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # ocr
+    text = pytesseract.image_to_string(frame)
+    print('='*10)
+    print(text)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # fps
+    sleep(1)
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
